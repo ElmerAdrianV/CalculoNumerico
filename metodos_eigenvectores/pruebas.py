443ab3c1d3663b66b018adbe7a67635d43c157b3
elmeradrianv <elmeradrianv@outlook.com>
2022-08-23 10:08:54 -0500

    Comprobando funcionalidad

--- metodos_eigenvectores/pruebas.py
+++ metodos_eigenvectores/pruebas.py
@@ -1,8 +1,7 @@
-from re import M
-from tkinter import E
+#%%
 import numpy as np
 import metodos_eigenvectores as met_eigen
-
+#%%
 # Pruebas de los métodos
 diag = np.trunc(10 * np.random.rand(3, 3))
 diag = np.diag(np.diag(diag))
@@ -12,3 +11,9 @@ eigenvalores = np.diag(diag)
 print(eigenvalores)
 print(met_eigen.metodoPotencia(A,np.array([1,1,1]),1000,10**-6))
 print(np.linalg.eig(A))
+#%%
+A = np.array([[11,18],[3,14]])
+print(A)
+print(met_eigen.metodoPotenciaInv(A,np.array([1,1]),1000,10**-6))
+
+# %%
