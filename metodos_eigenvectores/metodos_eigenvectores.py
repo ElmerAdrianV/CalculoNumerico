f6d4f091ee791dc14f70314cbe1a3a5a672c98c4
elmeradrianv <elmeradrianv@outlook.com>
2022-08-23 10:08:32 -0500

    Corrigiendo Bugs

--- metodos_eigenvectores/metodos_eigenvectores.py
+++ metodos_eigenvectores/metodos_eigenvectores.py
@@ -14,12 +14,10 @@ def metodoPotencia(A, q_0, m, tol) -> tuple:
     q_i = q_0/np.linalg.norm(q_0, np.inf)
     iteration = 0
     error = np.inf
-
     while iteration < m and tol < error:
         q_i_1 = A@q_i
         sigmai = np.linalg.norm(q_i_1, np.inf)
         q_i_1 = q_i_1/sigmai
-
         error = np.linalg.norm(q_i_1 - q_i, np.inf) / \
             np.linalg.norm(q_i_1, np.inf)
         iteration += 1
@@ -46,12 +44,10 @@ def metodoPotenciaInv(A, q_0, m, tol) -> tuple:
         q_i_1 = np.linalg.solve(A, q_i)
         sigmai = np.linalg.norm(q_i_1, np.inf)
         q_i_1 = q_i_1/sigmai
-
         error = np.linalg.norm(q_i_1 - q_i, np.inf) / \
             np.linalg.norm(q_i_1, np.inf)
         iteration += 1
         q_i = q_i_1
-
     return sigmai, q_i
 
 
